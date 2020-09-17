0e8ca7b26e85afb771708e51a35a728b4926a3c5
abhhi <57216753+birdawade11@users.noreply.github.com>
2020-09-17 17:36:26 +0530

    Update nscan.sh

--- nscan.sh
+++ nscan.sh
@@ -3,7 +3,7 @@
 domain=$1
 mkdir -p ~/projects/$domain/scan/nuclei
 probed="~/projects/$domain/domains/probed.txt"
-scan="~/projects/$domain/scan"
+scan="~/projects/$domain/scan/nuclei"
 
 
 
@@ -15,19 +15,33 @@ echo "##################       ------        ####################"
 echo "----------------------------------------------------------"
 
 
-~/code-a-thon/nuclei/cmd/nuclei/nuclei -c 200 -silent -l ~/projects/$domain/domains/probed.txt -t ~/code-a-thon/nuclei-templates/cves -o $scan/nuclie/cves
-~/code-a-thon/nuclei/cmd/nuclei/nuclei -c 200 -silent -l ~/projects/$domain/domains/probed.txt -t ~/code-a-thon/nuclei-templates/dns -o $scan/nuclie/dns
-~/code-a-thon/nuclei/cmd/nuclei/nuclei -c 200 -silent -l ~/projects/$domain/domains/probed.txt -t ~/code-a-thon/nuclei-templates/generic-detections -o $scan/nuclie/generic-detections		
-~/code-a-thon/nuclei/cmd/nuclei/nuclei -c 200 -silent -l ~/projects/$domain/domains/probed.txt -t ~/code-a-thon/nuclei-templates/panels -o $scan/nuclie/panels	
-~/code-a-thon/nuclei/cmd/nuclei/nuclei -c 200 -silent -l ~/projects/$domain/domains/probed.txt -t ~/code-a-thon/nuclei-templates/subdomain-takeover	 -o $scan/nuclie/subdomain-takeover
-~/code-a-thon/nuclei/cmd/nuclei/nuclei -c 200 -silent -l ~/projects/$domain/domains/probed.txt -t ~/code-a-thon/nuclei-templates/tokens -o $scan/nuclie/tokens
-~/code-a-thon/nuclei/cmd/nuclei/nuclei -c 200 -silent -l ~/projects/$domain/domains/probed.txt -t ~/code-a-thon/nuclei-templates/workflows	 -o $scan/nuclie/workflows
-~/code-a-thon/nuclei/cmd/nuclei/nuclei -c 200 -silent -l ~/projects/$domain/domains/probed.txt -t ~/code-a-thon/nuclei-templates/default-credentials -o $scan/nuclie/default-credentials
-~/code-a-thon/nuclei/cmd/nuclei/nuclei -c 200 -silent -l ~/projects/$domain/domains/probed.txt -t ~/code-a-thon/nuclei-templates/files -o $scan/nuclie/files
-~/code-a-thon/nuclei/cmd/nuclei/nuclei -c 200 -silent -l ~/projects/$domain/domains/probed.txt -t ~/code-a-thon/nuclei-templates/security-misconfiguration -o $scan/nuclie/security-misconfiguration
-~/code-a-thon/nuclei/cmd/nuclei/nuclei -c 200 -silent -l ~/projects/$domain/domains/probed.txt -t ~/code-a-thon/nuclei-templates/payloads -o $scan/nuclie/payloads
-~/code-a-thon/nuclei/cmd/nuclei/nuclei -c 200 -silent -l ~/projects/$domain/domains/probed.txt -t ~/code-a-thon/nuclei-templates/technologies -o $scan/nuclie/technologies	
-~/code-a-thon/nuclei/cmd/nuclei/nuclei -c 200 -silent -l ~/projects/$domain/domains/probed.txt -t ~/code-a-thon/nuclei-templates/vulnerabilities -o $scan/nuclie/vulnerabilities
+~/code-a-thon/nuclei/cmd/nuclei/nuclei -c 200 -silent -l ~/projects/$domain/domains/probed.txt -t ~/code-a-thon/nuclei-templates/cves -o cves
+~/code-a-thon/nuclei/cmd/nuclei/nuclei -c 200 -silent -l ~/projects/$domain/domains/probed.txt -t ~/code-a-thon/nuclei-templates/dns -o dns
+~/code-a-thon/nuclei/cmd/nuclei/nuclei -c 200 -silent -l ~/projects/$domain/domains/probed.txt -t ~/code-a-thon/nuclei-templates/generic-detections -o generic-detection
+~/code-a-thon/nuclei/cmd/nuclei/nuclei -c 200 -silent -l ~/projects/$domain/domains/probed.txt -t ~/code-a-thon/nuclei-templates/panels -o panels	
+~/code-a-thon/nuclei/cmd/nuclei/nuclei -c 200 -silent -l ~/projects/$domain/domains/probed.txt -t ~/code-a-thon/nuclei-templates/subdomain-takeover	 -o subdomain-takeover
+~/code-a-thon/nuclei/cmd/nuclei/nuclei -c 200 -silent -l ~/projects/$domain/domains/probed.txt -t ~/code-a-thon/nuclei-templates/tokens -o tokens
+~/code-a-thon/nuclei/cmd/nuclei/nuclei -c 200 -silent -l ~/projects/$domain/domains/probed.txt -t ~/code-a-thon/nuclei-templates/workflows	 -o workflows
+~/code-a-thon/nuclei/cmd/nuclei/nuclei -c 200 -silent -l ~/projects/$domain/domains/probed.txt -t ~/code-a-thon/nuclei-templates/default-credentials -o default-credentials
+~/code-a-thon/nuclei/cmd/nuclei/nuclei -c 200 -silent -l ~/projects/$domain/domains/probed.txt -t ~/code-a-thon/nuclei-templates/files -o files
+~/code-a-thon/nuclei/cmd/nuclei/nuclei -c 200 -silent -l ~/projects/$domain/domains/probed.txt -t ~/code-a-thon/nuclei-templates/security-misconfiguration -o security-misconfiguration
+~/code-a-thon/nuclei/cmd/nuclei/nuclei -c 200 -silent -l ~/projects/$domain/domains/probed.txt -t ~/code-a-thon/nuclei-templates/payloads -o payloads
+~/code-a-thon/nuclei/cmd/nuclei/nuclei -c 200 -silent -l ~/projects/$domain/domains/probed.txt -t ~/code-a-thon/nuclei-templates/technologies -o technologies	
+~/code-a-thon/nuclei/cmd/nuclei/nuclei -c 200 -silent -l ~/projects/$domain/domains/probed.txt -t ~/code-a-thon/nuclei-templates/vulnerabilities -o vulnerabilities
+
+mv cves $scan/cve
+mv dns $scan/dns
+mv generic-detection $scan/generic-detection
+mv panels $scan/panels
+mv subdomain-takeover $scan/subdomain-takeover
+mv tokens $scan/tokens
+mv workflows $scan/workflows
+mv default-credentials $scan/default-credentials
+mv files $scan/files
+mv security-misconfiguration $scan/security-misconfiguratione
+mv payloads $scan/payloads
+mv cves $scan/cve
+mv technologies $scan/technologies
 
 
 echo "----------------------------------------------------------"
