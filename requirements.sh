af3c04bd79e0baa433c27a5c406948ce81bd95ae
abhhi <57216753+birdawade11@users.noreply.github.com>
2020-09-17 14:43:08 +0530

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,38 +1,37 @@
 #!/bin/env bash
 
-su 
-apt-get update
-apt-get install golang
-apt-get install git
+sudo apt-get update
+sudo apt-get install golang
+sudo apt-get install git
 
 git clone https://github.com/projectdiscovery/shuffledns.git
 cd shuffledns/cmd/shuffledns
-go build main.go
+sudo go build main.go
 mv main shuffledns
 cd
 
 git clone https://github.com/projectdiscovery/httpx.git
 cd httpx/cmd/httpx
-go build httpx.go
+sudo go build httpx.go
 cd
 
 git clone https://github.com/projectdiscovery/subfinder.git
 cd subfinder/cmd/subfinder/
-go build main.go
+sudo go build main.go
 mv main subfinder
 
-wget https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip
+sudo wget https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip
 unzip aquatone_linux_amd64_1.7.0.zip
 
 git clone https://github.com/projectdiscovery/nuclei.git
 cd nuclei/v2/cmd/nuclei
-go build main.go
+sudo go build main.go
 mv main.go nuclei
 
 git clone https://github.com/projectdiscovery/nuclei-templates.git
 
 git clone https://github.com/projectdiscovery/naabu.git
 cd naabu/cmd/naabu
-go build main.go
+sudo go build main.go
 mv main naabu
 
