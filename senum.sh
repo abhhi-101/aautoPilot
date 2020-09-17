212cadf5a9e7589fb30dce24785c9bd5fdbe0a14
abhhi <57216753+birdawade11@users.noreply.github.com>
2020-09-17 16:17:20 +0530

    Update senum.sh

--- senum.sh
+++ senum.sh
@@ -18,18 +18,18 @@ echo "----------------------------------------------------------"
 
 subdomain_enum(){
 
-sudo sublist3r -d $domain -t 50 -p 80,443 -n | awk '/ - / {print$1}' | tee  $domains/sublister.txt
-~/code-a-thon/subfinder/cmd/subfinder/subfinder -d $domain  -max-time 2 -nC -nW  | tee $domains/subfinder.txt
-curl -s https://certspotter.com/api/v0/certs\?domain\=$domain | jq '.[].dns_names[]' | sed 's/\"//g' | sed 's/\*\.//g' | sort -u | grep $domain  | tee $domains/certspotter.txt
-timeout 3m amass enum --passive -silent -d $domain | tee $domains/amass.txt
-cat $domains/*.txt | sort | uniq | tee $domains/all.txt
+sudo sublist3r -d $domain -t 50 -p 80,443 -n | awk '/ - / {print$1}' | tee  ~/projects/$domain/domains/sublister.txt
+~/code-a-thon/subfinder/cmd/subfinder/subfinder -d $domain  -max-time 2 -nC -nW  | tee ~/projects/$domain/domains/subfinder.txt
+curl -s https://certspotter.com/api/v0/certs\?domain\=$domain | jq '.[].dns_names[]' | sed 's/\"//g' | sed 's/\*\.//g' | sort -u | grep $domain  | tee ~/projects/$domain/domains/certspotter.txt
+timeout 3m amass enum --passive -silent -d $domain | tee ~/projects/$domain/domains/amass.txt
+cat ~/projects/$domain/domains/*.txt | sort | uniq | tee ~/projects/$domain/domains/all.txt
 rm -r $domains/sublister.txt
 rm -r $domains/subfinder.txt
 rm -r $domains/certspotter.txt
 rm -r $domains/amass.txt
 
-~/code-a-thon/shuffledns/cmd/shuffledns/shuffledns -list $domains/all.txt -r $resolver -massdns $massdns | tee $domains/domain.txt
-~/code-a-thon/httpx/cmd/httpx/httpx -silent -websocket -l $domains/domain.txt | tee $domains/probed.txt
+~/code-a-thon/shuffledns/cmd/shuffledns/shuffledns -list $domains/all.txt -r $resolver -massdns $massdns | tee ~/projects/$domain/domains/domain.txt
+~/code-a-thon/httpx/cmd/httpx/httpx -silent -websocket -l $domains/domain.txt | tee ~/projects/$domain/domains/probed.txt
 
 
 
