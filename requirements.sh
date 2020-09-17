73e7f4c47cc155f241892a8b0bd6be014fa301fb
abhhi <57216753+birdawade11@users.noreply.github.com>
2020-09-17 15:53:26 +0530

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -11,13 +11,13 @@ cd shuffledns/cmd/shuffledns
 sudo go build main.go
 mv main shuffledns
 cd
-cd code-a-thon
+cd ~/code-a-thon/
 echo "##########Installing httpx##########"
 git clone https://github.com/projectdiscovery/httpx.git
 cd httpx/cmd/httpx
 sudo go build httpx.go
 cd
-cd code-a-thon
+cd ~/code-a-thon/
 echo "##########Installing subfinder##########"
 
 git clone https://github.com/projectdiscovery/subfinder.git
@@ -25,13 +25,13 @@ cd subfinder/cmd/subfinder/
 sudo go build main.go
 mv main subfinder
 cd
-cd code-a-thon
+cd ~/code-a-thon/
 echo "##########Installing aquatone##########"
 
 sudo wget https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip
 unzip aquatone_linux_amd64_1.7.0.zip
 cd
-cd code-a-thon
+cd ~/code-a-thon/
 echo "##########Installing nuclei##########"
 
 git clone https://github.com/projectdiscovery/nuclei.git
@@ -39,12 +39,12 @@ cd nuclei/v2/cmd/nuclei
 sudo go build main.go
 mv main.go nuclei
 cd
-cd code-a-thon
+cd ~/code-a-thon/
 echo "##########Installing nuclei-templates##########"
 
 git clone https://github.com/projectdiscovery/nuclei-templates.git
 cd
-cd code-a-thon
+cd ~/code-a-thon/
 echo "##########Installing naabu##########"
 
 git clone https://github.com/projectdiscovery/naabu.git
@@ -52,5 +52,5 @@ cd naabu/cmd/naabu
 sudo go build main.go
 mv main naabu
 cd
-cd code-a-thon
+cd ~/code-a-thon/
 
